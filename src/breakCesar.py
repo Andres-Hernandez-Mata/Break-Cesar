04079f074e554f58a4aee4a40f807d43775c26c9
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-24 01:27:35 -0500

    Validar cadena
    Version 1.5..0

--- src/breakCesar.py
+++ src/breakCesar.py
@@ -1,7 +1,7 @@
 """
 Uso: Rompe el cifrado César usando brute force
 Creado: Andrés Hernández Mata
-Version: 1.0.0
+Version: 1.5.0
 Python: 3.9.1
 Fecha: 22 Abril 2020
 """
@@ -16,22 +16,32 @@ os.system('cls')
    
 banner = header.figlet_format('BREAK CESAR').upper()
 print(colored(banner.rstrip('\n'), 'blue', attrs=['bold']))
-print(colored('         By Andrés Hernández Mata | Versión 1.0.0 | LSTI \n', 'red', attrs=['bold']))
-   
-mensaje = input('Ingresa la cadena: ')
-simbolos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'    
+print(colored('         By Andrés Hernández Mata | Versión 1.5.0 | LSTI \n', 'red', attrs=['bold']))
 
-for llave in range(len(simbolos)):
-    resultado = ''
-    for simbolo in mensaje:
-        if simbolo in simbolos:
-            indice_simbolo = simbolos.find(simbolo)
-            indice_nuevo = indice_simbolo - llave                
-            if indice_nuevo < 0:
-                indice_nuevo = indice_nuevo + len(simbolos)                                
-            resultado = resultado + simbolos[indice_nuevo]    
-        else:
-            resultado = resultado + simbolo
-    print('Llave #%s: %s' % (llave, resultado))
+try:
+    while True:        
+        mensaje = input('Ingresa la cadena: ')
+        if mensaje != '':
+            break
+except Exception as error:    
+    print(colored(error, 'red', attrs=['bold']))
 
+simbolos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+
+try:
+    for llave in range(len(simbolos)):
+        resultado = ''
+        for simbolo in mensaje:
+            if simbolo in simbolos:
+                indice_simbolo = simbolos.find(simbolo)
+                indice_nuevo = indice_simbolo - llave                
+                if indice_nuevo < 0:
+                    indice_nuevo = indice_nuevo + len(simbolos)
+                resultado = resultado + simbolos[indice_nuevo]    
+            else:
+                resultado = resultado + simbolo
+        
+        print('Llave #%s: %s' % (llave, resultado))
+except Exception as error:
+    print(colored(error, 'red', attrs=['bold']))
 
