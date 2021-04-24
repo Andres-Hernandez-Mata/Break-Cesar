fb0e8d1c88e231a88af13b5c38029ad19ccabc3a
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-24 09:36:42 -0500

    Eliminar espacios
    Creador y espacios

--- src/breakCesar.py
+++ src/breakCesar.py
@@ -1,6 +1,6 @@
 """
 Uso: Rompe el cifrado César usando brute force
-Creado: Andrés Hernández Mata
+Creador: Andrés Hernández Mata
 Version: 1.5.0
 Python: 3.9.1
 Fecha: 22 Abril 2020
@@ -13,18 +13,18 @@ import pyfiglet as header
 from termcolor import colored
 
 os.system('cls')
-   
+
 banner = header.figlet_format('BREAK CESAR').upper()
 print(colored(banner.rstrip('\n'), 'blue', attrs=['bold']))
 print(colored('         By Andrés Hernández Mata | Versión 1.5.0 | LSTI \n', 'red', attrs=['bold']))
 
 try:
-    while True:        
+    while True:
         mensaje = input('Ingresa la cadena: ')
         if mensaje != '':
             break
         print(colored('La cadena es un campo obligatorio', 'red', attrs=['bold']))
-except Exception as error:    
+except Exception as error:
     print(colored(error, 'red', attrs=['bold']))
 
 simbolos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
@@ -35,14 +35,13 @@ try:
         for simbolo in mensaje:
             if simbolo in simbolos:
                 indice_simbolo = simbolos.find(simbolo)
-                indice_nuevo = indice_simbolo - llave                
+                indice_nuevo = indice_simbolo - llave
                 if indice_nuevo < 0:
                     indice_nuevo = indice_nuevo + len(simbolos)
-                resultado = resultado + simbolos[indice_nuevo]    
+                resultado = resultado + simbolos[indice_nuevo]
             else:
                 resultado = resultado + simbolo
-        
+
         print('Llave #%s: %s' % (llave, resultado))
 except Exception as error:
     print(colored(error, 'red', attrs=['bold']))
-
